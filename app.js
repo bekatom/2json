3bff24d5590c4b1b2221d4cd168eb6473f950c46
btomashvili <bekatomashvili@gmail.com>
2015-06-27 21:58:50 +0400

    bootstrap forms

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var multer  = require('multer');
-var routes = require('./routes/index');
-var users = require('./routes/users');
 var swig  = require('swig');
 
 
@@ -17,7 +15,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'swig');
 
 // uncomment after placing your favicon in /public
-app.use(favicon(__dirname + '/public/favicon.ico'));
+//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
